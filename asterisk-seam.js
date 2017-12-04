fcd984693fcef7e530e3774338fd96b6b8e3979f
cvtk <olegcvetkov76@gmail.com>
2017-12-04 21:18:09 +0300

    add lowdb

--- asterisk-seam.js
+++ asterisk-seam.js
@@ -6,25 +6,59 @@ const express = require('express');
 const Bot = require('node-telegram-bot-api');
 const R = require('ramda');
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const db = low( new FileSync('db.json') );
+
 const app = express();
 const bot = new Bot( '462050712:AAFUu-f72XZ2tYwsQeSuLNUFAbNoB7O7prU', { polling: true } );
 const chatId = '-294390961';
 const token = '5274ygs1BFVps9w37F8B';
 
+const _html = {
+  a: function (text, link) {
+    return `<a href="${link}">${text}</a>`;
+  },
+  b: function(text) {
+    return `<strong>${text}</strong>`;
+  }
+}
+
 function isForbiddenRequest(tkn) {
   return R.isEmpty(tkn) || tkn !== token;
 };
 
-function _a(text, link) {
-  return `<a href="${link}">${text}</a>`;
+function _strToPhone(str) {
+  return str.replace(/\D/g,'').slice(-10);
 };
 
-function _b(text) {
-  return `<strong>${text}</strong>`;
+function _unixDate() {
+  return Math.floor(Date.now() / 1000);
 }
 
+function _salt() {
+  let now = new Date(),
+      startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  return (startOfDay / 1000).toString;
+};
+
+function _id(str, salt) {
+  let hash = 0, i, chr;
+
+  str += salt;
+  
+  for (i = 0; i < str.length; i++) {
+    chr   = str.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0;
+  }
+  return hash;
+};
+
 app.get('/incoming-call', function (req, res) {
-  let q = req.query;
+  let q = req.query,
+      call = {};
+
   if ( isForbiddenRequest(q.token) ) {
     res.status(403).json( { err: 'forbidden, no valid token' } );
     return;
@@ -40,8 +74,21 @@ app.get('/incoming-call', function (req, res) {
     return;
   };
 
+  call = {
+    from: _strToPhone(q.phoneNumber),
+    id: _id( q.callId, _salt() ),
+    created: _unixDate(),
+    startAt: '',
+    endAt: '',
+    to: ''
+  };
+
+  db.defaults({ calls: [] }).write();
+
+  db.get('calls').push(call).write();
+
   res.status(200).json('200');
-  bot.sendMessage(chatId, `${ _b('Входящий') } звонок от  ${ _a(q.contactName, q.contactLink) } ...`, { parse_mode: 'html' } );
+  bot.sendMessage(chatId, `${ _html.b('Входящий') } звонок от  ${ _html.a(call.from, 'http:/770760.ru') } ...`, { parse_mode: 'html' } );
 });
 
 app.listen(3000);
