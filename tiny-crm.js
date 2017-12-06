8bae5d6e9317051f60066aa738e94a2ff891b8b4
cvtk <olegcvetkov76@gmail.com>
2017-12-06 21:56:39 +0300

    add contact remove fn

--- tiny-crm.js
+++ tiny-crm.js
@@ -23,6 +23,16 @@ function _unixDate() {
   return Math.floor(Date.now() / 1000);
 };
 
+function createContact(name) {
+  const contact = {
+      id: _id.generate(),
+      created: _unixDate(),
+      name: name
+    }
+    db.get('contacts').push(contact).write();
+    return contact.id;
+};
+
 function contactsHandler(msg, args) {
   const chatId = msg.chat.id,
         options = { parse_mode: 'html' };
@@ -34,27 +44,56 @@ function contactsHandler(msg, args) {
   }
 };
 
-function editContactHandler(msg, contactId) {
-  const chatId = msg.chat.id, userId = msg.from.id;
+function removeContactConfirmHandler(msg, contactId) {
+  const chatId = msg.chat.id;
   const opt = {
     parse_mode: 'html',
     reply_markup: JSON.stringify({
       inline_keyboard: [[
-        { text: 'Рабочий тел.', callback_data: 'edit_contact_phone' },
-        { text: 'Мобильный тел.', callback_data: 'edit_contact_mobile' },
-        { text: 'Заметка', callback_data: 'edit_contact_note' }
-      ]]
+        { text: '\u2757 Я уверен, что хочу удалить', callback_data: `remove_contact__${ contactId }` }
+      ]],
     })
   };
-  state.setCurrent(userId) === 'edit_contact'
+  bot.sendMessage(chatId, `Я не люблю насилие, ${ msg.chat.first_name }. Я бизнесмен. Кровь — непозволительная роскошь`, opt );
+};
+
+function removeContactHandler(msg, contactId) {
+  const chatId = msg.chat.id;
   const contact = db.get('contacts').find({ id: contactId }).value();
-  const message = `Сделайте ему предложение, от которого он не сможет отказаться.\n<strong>Имя:</strong><i>${ contact.name }</i>\n<strong>Рабочий:</strong> <i>${ contact.phone || 'нет' }</i>\n<strong>Мобильный:</strong> <i>${ contact.mobile || 'нет' }</i>\n<pre>${ contact.note || 'нет заметок' }</pre>`;
-  bot.sendMessage(chatId, message, opt );
+  if ( !contact ) return;
+  db.get('contacts').remove({ id: contactId }).write();
+  bot.sendMessage(chatId, `\uD83D\uDC1F Это сицилийский знак. Он означает, что ${ contact.name } кормит рыб на дне океана` );
 }
+
+function editContactHandler(msg, contactId) {
+  const chatId = msg.chat.id;
+  const opt = {
+    parse_mode: 'html',
+    reply_markup: JSON.stringify({
+      inline_keyboard: [[
+        { text: 'Рабочий тел.', callback_data: `edit_contact_phone__${ contactId }` },
+        { text: 'Личный тел.', callback_data: `edit_contact_mobile__${ contactId }` }],
+        [{ text: 'Заметка', callback_data: `edit_contact_note__${ contactId }` },
+        { text: 'Удалить', callback_data: `remove_contact_confirm__${ contactId }` }]]
+    })
+  };
+  const contact = db.get('contacts').find({ id: contactId }).value();
+  const message = `Сделай ему предложение, от которого он не сможет отказаться\n<strong>Имя:</strong> ${ contact.name }\n<strong>Рабочий:</strong> ${ contact.phone || '\uD83D\uDEAB' }\n<strong>Личный:</strong> ${ contact.mobile || '\uD83D\uDEAB' }\n${ contact.note? '<pre>' + contact.note + '</pre>': '' }`;
+  bot.sendMessage(chatId, message, opt );
+};
+
+function createContactWNameHandler(msg, args) {
+  const userId = msg.from.id;
+  console.log(args);
+
+    // const contactId = createContact(msg.text);
+    // state.setCurrent(userId) === '';
+    // editContactHandler(msg, contactId);
+};
 
 function createContactHandler(msg, args) {
   const chatId = msg.chat.id, opt = { parse_mode: 'html' };
-  bot.sendMessage(chatId, 'Просто <strong>назови имя</strong> этого человека, о нем позаботятся',  opt )
+  bot.sendMessage(chatId, 'Просто <strong>назови имя</strong> этого человека и о нем позаботятся',  opt )
       .then( data => state.setCurrent(data.chat.id, 'new_contact_name'));
 }
 
@@ -77,24 +116,28 @@ function createHandler(msg, args) {
 
 function requestsHandler(msg) {
   const userId = msg.from.id;
-  console.log(msg);
   if ( state.getCurrent(userId) === 'new_contact_name') {
-    const contact = {
-      id: _id.generate(),
-      created: _unixDate(),
-      name: msg.text
-    }
-    db.get('contacts').push(contact).write();
-    editContactHandler(msg, contact.id);
+    const contactId = createContact(msg.text);
+    state.setCurrent(userId) === '';
+    editContactHandler(msg, contactId);
   }
 };
 
 function callbackQueryHandler(msg) {
-  if ( msg.data === 'new_contact') { createContactHandler(msg.message) }
+  const [ type, objectId ] = msg.data.split('__');
+  switch( type ) {
+    case 'new_contact': createContactHandler(msg.message); break;
+    case 'remove_contact_confirm': removeContactConfirmHandler(msg.message, objectId); break;
+    case 'remove_contact': removeContactHandler(msg.message, objectId); break;
+    //case 'edit_contact_phone': createContactHandler(msg.message); break;
+    //case 'edit_contact_mobile': createContactHandler(msg.message); break;
+    //case 'edit_contact_note': createContactHandler(msg.message); break;
+  }
 };
 
 bot.onText(/(\/|\.)(con|кон|rjy|сщт)\S*(.*)/i, contactsHandler );
 bot.onText( _re(['create']), createHandler );
 bot.onText( _re(['create', 'sp', 'contact']), createContactHandler );
+bot.onText( _re(['create', 'sp', 'contact', 'sp', 'name']), createContactWNameHandler );
 bot.on( 'message', requestsHandler );
 bot.on( 'callback_query', callbackQueryHandler );
